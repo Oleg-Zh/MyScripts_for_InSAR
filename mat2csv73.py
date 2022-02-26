e3eca97251b2a7eba21055ac7a438e29b5c62f16
Oleg-Zh <38970185+Oleg-Zh@users.noreply.github.com>
2022-02-26 18:20:33 +0700

    Add files via upload

---
+++ mat2csv73.py
@@ -0,0 +1,40 @@
+import sys
+import pathlib
+from scipy.io import loadmat
+import h5py
+import pandas as pd
+import numpy as np
+from datetime import date, timedelta, datetime
+
+# определение пути
+currentDirectory = pathlib.Path('.')
+
+# определение шаблона
+currentPattern = "ps_plot_ts*.mat"
+
+# конвертирование в формат csv
+for inputfile in currentDirectory.glob(currentPattern):
+    outputfile = str(inputfile)[:-3] + 'csv'
+    ts = h5py.File(inputfile)
+    def from_mat_date(md):
+        return str(date.fromordinal(int(md)) + timedelta(days=md%1) - timedelta(days = 366))
+
+    columns_date = [(lambda i: from_mat_date(int(i)))(i) for i in list(ts['day'][0])]
+
+    x = np.array(list(ts['ph_mm'])).T
+    df = pd.DataFrame(x,columns=columns_date)
+    dstat = pd.DataFrame()
+    dstat['mean'] = df.apply(np.mean, axis=1)
+    dstat['median'] = df.apply(np.median, axis=1)
+    dstat['std'] = df.apply(np.std, axis=1)
+    dstat['average'] = df.apply(np.average, axis=1)
+
+    d0 = pd.concat([df, dstat], axis=1)
+
+    columns = ['longitude', 'latitude']
+    x = np.array(list(ts['lonlat'])).T
+    d1 = pd.DataFrame(x, columns=columns)
+
+    df = pd.concat([d1,d0], axis=1)
+    df.to_csv(outputfile, index=False)
+    print('File {} has been converting and writing to {}'.format(inputfile, outputfile))
